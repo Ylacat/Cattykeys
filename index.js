ba85eed03a0edc9c57e6b11ffc10db8184d61935
Frederick Nicklas Ambo Eggert Eggertsen <frederick@tacaly.com>
2021-05-09 18:07:38 +0200

    Added lock system module
    Added https://github.com/sindresorhus/lock-system

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 // Created by Tacaly - aka Frederick Ambo
+const lockSystem = require('lock-system');
 
 if (window.focus == true){
     enableKeys()
 }
 if (window.focus == false){
     disableKeys()
+    lockSystem();
 }
